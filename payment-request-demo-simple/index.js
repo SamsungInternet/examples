57b9245f85e4e9684f41058165819953aa9f602b
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2017-03-14 11:43:54 +0000

    Update simple payment demo with new format

--- payment-request-demo-simple/index.js
+++ payment-request-demo-simple/index.js
@@ -5,8 +5,21 @@ var unsupportedMsg = document.getElementById('unsupported');
 var successMsg = document.getElementById('success');
 var errorMsg = document.getElementById('error');
 
-// Configuration for our payment (other payment providers are available too 😄)
-var methodData = [{supportedMethods: ['visa', 'mastercard', 'amex']}];
+/**
+ * Configuration for our payment. Notes:
+ *   - basic-card: We're taking a card payment. Other options should come in the future.
+ *   - We duplicate the payment network names in supportedMethods, to support older format (Samsung Internet v5.0)
+ *   - Samsung Internet (current latest v5.4) does not yet support 'supportedTypes'
+ *   - These are example payment networks. Others are available! See:
+ *     https://developers.google.com/web/fundamentals/discovery-and-monetization/payment-request/#methoddata-parameter
+ */
+var methodData = [{
+  supportedMethods: ['basic-card', 'visa', 'mastercard', 'amex'],
+  data: {
+    supportedNetworks: ['visa', 'mastercard', 'amex']
+  }
+}];
+
 var details = {total: {label: 'Test payment', amount: {currency: 'GBP', value: '1.00'}}};
 
 // Check that the Payment Request API is available on this device
